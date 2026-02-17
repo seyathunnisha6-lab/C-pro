0276913846cf3b4195492fc772477c1cec145c7d
seyathunnisha6-lab <seyathunnisha6@gmail.com>
2026-02-17 16:24:15 +0530

    Add Factorial program in C

---
+++ Conditional statements/Factorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    long fact = 1;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    for(i=1; i<=n; i++)
+        fact *= i;
+
+    printf("Factorial = %ld", fact);
+
+    return 0;
+}
